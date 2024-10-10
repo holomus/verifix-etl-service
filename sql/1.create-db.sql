1e6e5b7471060e60940788244d06c7fc387b79ac
holomus <leoabrahaam@gmail.com>
2024-10-10 09:21:53 +0500

    initial

---
+++ sql/1.create-db.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE verifix_etl_db;
+\connect verifix_etl_db
---
+++ sql/2.create-user.sql
@@ -0,0 +1,12 @@
+-- Prompt user for input for the username and password
+\prompt 'Enter username: ' username
+\prompt 'Enter password: ' password
+
+-- Create the user
+CREATE ROLE :username WITH LOGIN PASSWORD ':password';
+
+-- Grant role privileges
+GRANT ALL PRIVILEGES ON DATABASE verifix_etl_db TO :username;
+
+-- Create a schema
+CREATE SCHEMA IF NOT EXISTS verifix AUTHORIZATION :username;
