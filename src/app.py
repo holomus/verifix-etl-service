ec678017aa02d3b59358c2915c08acb3e09615a5
holomus <leoabrahaam@gmail.com>
2024-10-29 14:08:01 +0500

    change

--- src/app.py
+++ src/app.py
@@ -26,12 +26,12 @@ def verify_user(
   credentials: Annotated[HTTPBasicCredentials, Depends(security)],
 ):
   current_username_bytes = credentials.username.encode("utf8")
-  correct_username_bytes = b"admin"
+  correct_username_bytes = b"admin" # TODO: add to config
   is_correct_username = secrets.compare_digest(
     current_username_bytes, correct_username_bytes
   )
   current_password_bytes = credentials.password.encode("utf8")
-  correct_password_bytes = b"admin"
+  correct_password_bytes = b"admin" # TODO: add to config
   is_correct_password = secrets.compare_digest(
     current_password_bytes, correct_password_bytes
   )
